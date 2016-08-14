071f46bb9719968f3f3a8b8a55f5f43f82c14faf
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-08-14 12:56:50 -0400

    got tasks to update on server

---
+++ mock.js
@@ -0,0 +1,37 @@
+var data = [
+  {
+    id: 1,
+    attributes: {
+      todo: "Get gas",
+      isComplete: false
+    }
+  },
+  {
+    id: 2,
+    attributes: {
+      todo: "Do homework",
+      isComplete: false
+    }
+  },
+  {
+    id: 3,
+    attributes: {
+      todo: "Groceries",
+      isComplete: true
+    }
+  },
+  {
+    id: 4,
+    attributes: {
+      todo: "Learn stuff",
+      isComplete: false
+    }
+  },
+  {
+    id: 5,
+    attributes: {
+      todo: "complain",
+      isComplete: true
+    }
+  }
+];
